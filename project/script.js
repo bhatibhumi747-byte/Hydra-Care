d0a602aab0c963c229d57baa4e6b1dc5f6687edc
bhumi bhati <bhatibhumi747@gmail.com>
2026-01-05 11:30:26 +0530

    initial commit

--- project/script.js
+++ project/script.js
@@ -1,3 +1,15 @@
 function openApp() {
   window.open("app.html", "_blank");
 }
+
+const waterSound = new Audio('thailand-eas-alarm-2006-266492.mp3');
+
+
+function playMusic() {
+    waterSound.play();
+}
+
+
+window.onload = function() {
+    waterSound.play();
+}
